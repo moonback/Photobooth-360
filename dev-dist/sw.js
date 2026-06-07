21fc6456905c21904fb2ee5fffdb2c314fb71444
Medusa <devoye.maysson@gmail.com>
2026-06-07 21:16:24 +0200

    fff

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-80b25e96'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.5o95k1504k8"
+    "revision": "0.p9l31ckh9kk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
